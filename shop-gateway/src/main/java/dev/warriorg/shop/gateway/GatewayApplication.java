e70fe2837f68e459d62ef3a35058c7b5c61cf097
warriorg <warrior.gao@foxmail.com>
2022-03-20 19:25:43 +0800

    study gateway

---
+++ shop-gateway/src/main/java/dev/warriorg/shop/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package dev.warriorg.shop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
